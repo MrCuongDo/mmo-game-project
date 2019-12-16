25f617c9a47ef31decf6b95b08ad77d2819d02f6
CuongDo <cuongdoleviet@gmail.com>
2019-12-16 11:26:33 +0700

    websocket communication

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const serv = require('http').Server(app);
+// let server = require('http').Server(app);
+// let io = require('socket.io')(server);
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/client/index.html'));
 app.use('/client', express.static(__dirname + '/client'));
+app.use('/public', express.static(__dirname + '/public'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/client/index.html');
+});
+
+// set up for socket io
+let server = app.listen(port,function(){ // server start from here
+	console.log(`Example app listening on port ${port}!`)
+});
+let io = require('socket.io')(server);
+
+io.on('connection',function(socket){
+	console.log('socket connection');
+
+	//test
+	socket.on('cuong', function (data){
+		console.log('cuong' + data.reason);
+	});
+
+	socket.emit('serverTestMsg', {
+		msg: 'hello from server'
+	});
+});
