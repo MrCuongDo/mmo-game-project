4fb8af24999a4bc0edef18b24bd6896153bf1f27
CuongDo <cuongdoleviet@gmail.com>
2019-12-16 15:38:41 +0700

    keyboard interaction

--- index.js
+++ index.js
@@ -16,29 +16,78 @@ let server = app.listen(port,function(){ // server start from here
 	console.log(`Example app listening on port ${port}!`)
 });
 let io = require('socket.io')(server);
+let SOCKET_LIST = {};
 let PLAYER_LIST = {};
 
-io.on('connection',function(player){
+let Player = function(id){
+	let self = {
+		x: 250,
+		y: 250,
+		id: id,
+		number: '' +  Math.floor(Math.random() * 10),
+
+	}
+
+	self.maxMoveSpd = 10;
+	self.pressingUp= false;
+	self.pressingDown= false;
+	self.pressingLeft= false;
+	self.pressingRight= false;
+
+	self.updatePosition = function() {
+		if(self.pressingRight){
+			self.x += self.maxMoveSpd;
+		}
+		if(self.pressingLeft){
+			self.x -= self.maxMoveSpd;
+		}
+		if(self.pressingUp){
+			self.y -= self.maxMoveSpd;
+		}
+		if(self.pressingDown){
+			self.y += self.maxMoveSpd;
+		}
+	}
+
+	return self;
+}
+
+//===== SOCKET IO CONTROLLER ======
+io.on('connection',function(socket){
 	console.log('player connection');
 
-	player.id = Math.random();
-	player.x = 0;
-	player.y = 0;
-	player.number = '' + Math.floor( Math.random() * 10);
-	PLAYER_LIST[player.id] = player;
+	socket.id = Math.random();
+	SOCKET_LIST[socket.id] = socket;
 
-	player.on('disconnect',  function () {
-		delete PLAYER_LIST[player.id];
+	let player = Player(socket.id);
+	PLAYER_LIST[socket.id] = player;
+
+	socket.on('disconnect',  function () {
+		delete SOCKET_LIST[socket.id];
+		delete PLAYER_LIST[socket.id];
 	});
+
+	socket.on('keyPress',function(data){
+		if(data.inputId === 'left')
+			player.pressingLeft = data.state;
+		if(data.inputId === 'right')
+			player.pressingRight = data.state;
+		if(data.inputId === 'up')
+			player.pressingUp = data.state;
+		if(data.inputId === 'down')
+			player.pressingDown = data.state;
+	})
 
 });
 
+
+
+//===== MAIN APP ======
 setInterval(function() {
 	let pack = [];
 	for (let i in PLAYER_LIST){
 		let player = PLAYER_LIST[i];
-		player.x++;
-		player.y++;
+		player.updatePosition();
 		pack.push({
 			x: player.x,
 			y: player.y,
@@ -46,11 +95,11 @@ setInterval(function() {
 		})
 	}
 
-	for(let i in PLAYER_LIST){
-		let player = PLAYER_LIST[i];
-		player.emit('newPosition',pack)  // emit to client
+	for(let i in SOCKET_LIST){
+		let socket = SOCKET_LIST[i];
+		socket.emit('newPosition',pack)  // emit to client
 	}
 
-},1000/25);
+},1000/30);
 
 
