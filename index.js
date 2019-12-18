cd456495eb7dc86a3f235ba2349a72ab57570973
CuongDo <cuongdoleviet@gmail.com>
2019-12-18 14:09:07 +0700

    player attack and login system

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require('dotenv').config() // use for using of process.env
+
 const express = require('express');
 const app = express();
 const port = 3000;
+
+
 // let server = require('http').Server(app);
 // let io = require('socket.io')(server);
 
@@ -37,9 +41,64 @@ let Entity = function(){
 		self.y += self.spdY;
 	};
 
+	self.getDistance= function ( entity2) { // return distance (number)
+		let vx=self.x -entity2.x;
+		let vy = self.y - entity2.y;
+		return Math.sqrt(vx*vx + vy*vy);
+	}
+
+	self.testCollision = function (entity2){ // return if colliding (true/false)
+		let rect1 = {
+			x: self.x - self.width / 2,
+			y:  self.y - self.height / 2,
+			width: self.width,
+			height : self.height
+		}
+
+		let rect2 = {
+			x: entity2.x - entity2.width / 2,
+			y: entity2.y - entity2.height / 2,
+			width: entity2.width,
+			height : entity2.height
+		}
+		return testCollisionRectRect(rect1,rect2);
+	}
+
 	return self;
 }
 
+let USERS = {
+	'cuong':'123', 
+	'binh': '123',
+}
+
+// let isValidPassword = async function(data){
+// 	let rs = await setTimeout(function(){
+// 		return USERS[data.username] === data.password;
+// 	},10000);
+// 	console.log(rs);
+// }
+
+let isValidPassword = function(data,callback){
+	setTimeout(function(){
+		callback(USERS[data.username] === data.password) ;
+	},10);
+}
+
+let isUsernameTaken = function(data,callback){
+	setTimeout(function(){
+		callback(USERS[data.username]);
+	},10);
+	
+}
+let addUser = function(data,callback){
+	setTimeout(function(){
+		USERS[data.username] = data.password;
+		callback();
+	},10);
+}
+
+//-----------PLAYER ------------
 let Player = function(id){
 	let self = Entity();
 	self.id = id;
@@ -51,12 +110,28 @@ let Player = function(id){
 	self.pressingLeft= false;
 	self.pressingRight= false;
 
+	self.pressingAttack = false;
+	self.mouseAngle = 0;
+
+
 	let super_update = self.update;
 	self.update = function(){
 		self.updateSpd();
 		super_update();
+
+		if(self.pressingAttack) {
+			for(let i = -3 ; i < 3; i++){
+				self.shootBullet(i*10 + self.mouseAngle);
+			}
+		}
 	}
 
+	self.shootBullet = function(angel) {
+		let b = Bullet( self.id ,angel);
+		b.x = self.x;
+		b.y = self.y;
+	}
+	
 	self.updateSpd = function() {
 		if(self.pressingRight){
 			self.spdX = self.maxMoveSpd;
@@ -79,22 +154,30 @@ let Player = function(id){
 }
 
 Player.list = {};
+
 Player.onConnect = function(socket) {
 	let player = Player(socket.id);
 	socket.on('keyPress',function(data){
 		if(data.inputId === 'left')
 			player.pressingLeft = data.state;
-		if(data.inputId === 'right')
+		else if(data.inputId === 'right')
 			player.pressingRight = data.state;
-		if(data.inputId === 'up')
+		else if(data.inputId === 'up')
 			player.pressingUp = data.state;
-		if(data.inputId === 'down')
+		else if(data.inputId === 'down')
 			player.pressingDown = data.state;
-	})
+		else if(data.inputId === 'attack')
+			player.pressingAttack = data.state;
+		else if(data.inputId === 'mouseAngle')
+			player.mouseAngle = data.state;
+		
+	});
 }
+
 Player.onDisconnect = function(socket){
 	delete Player.list[socket.id];
 }
+
 Player.update = function() {
 	let pack = [];
 	for (let i in Player.list){
@@ -109,14 +192,13 @@ Player.update = function() {
 	return pack;
 }
 
-
 //----------BULLET------------
-let Bullet = function(angle){
+let Bullet = function(parent, angle){
 	let self = Entity();
 	self.id = Math.random();
 	self.spdX=Math.cos(angle/180*Math.PI) * 10;
 	self.spdY=Math.sin(angle/180*Math.PI) * 10;
-
+	self.parent = parent;
 	self.timer  =0;
 	self.toRemove = false;
 
@@ -126,6 +208,17 @@ let Bullet = function(angle){
 			self.toRemove = true;
 		}
 		super_update();
+
+		for(let i in Player.list){
+			let p = Player.list[i];
+			if (self.getDistance(p) < 32 && self.parent !== p.id) {
+				//handle collision here
+				/***
+				 * here
+				 */
+				self.toRemove = true;
+			}
+		}
 	}
 
 	Bullet.list[self.id] = self; // create new player automatically
@@ -135,18 +228,19 @@ let Bullet = function(angle){
 Bullet.list = {};
 
 Bullet.update = function(){
-	if(Math.random() < 0.1) {
-		Bullet(Math.random()* 360);
-	}
-
 	let pack = [];
 	for(let i in Bullet.list) {
 		let bullet = Bullet.list[i];
 		bullet.update();
-		pack.push({
-			x:bullet.x,
-			y:bullet.y
-		});
+		if(bullet.toRemove){
+			delete Bullet.list[i];
+		}else {
+			pack.push({
+				x:bullet.x,
+				y:bullet.y
+			});
+		}
+		
 	}
 	return pack;
 }
@@ -158,13 +252,52 @@ io.on('connection',function(socket){
 	socket.id = Math.random();
 	SOCKET_LIST[socket.id] = socket;
 
-	Player.onConnect(socket);
-	socket.on('disconnect',  function () {
+	socket.on('signIn', function(data){
+		isValidPassword(data, function(res){
+			if(res){
+				Player.onConnect(socket); // create user
+				socket.emit('signInResponse',{success : true});
+			}
+			else {
+				socket.emit('signInResponse',{success : false});
+			}
+		}); 
+	})
+
+	socket.on('signUp', function(data){
+		isUsernameTaken(data,function(res){
+			if(res) {
+				socket.emit('signUpResponse',{success : false});
+			}
+			else {
+				addUser(data, function(){
+					socket.emit('signUpResponse',{success : true});
+				});
+			}
+		});
+		
+	})
+	
+	socket.on('disconnect',function () {
 		delete SOCKET_LIST[socket.id];
 		Player.onDisconnect(socket);	
 	});
-});
 
+	socket.on('sendMsgToServer',function(data){
+		let playerName = (""+socket.id).slice(2,7);
+		for(let i in SOCKET_LIST) {
+			SOCKET_LIST[i].emit('addToChat',playerName + ': ' + data);
+		}
+	});
+
+	socket.on('evalMsg',function(data){
+		if(!process.env.DEBUG)
+			return; // if DBUG is false then return
+		let res = eval(data);
+		socket.emit('evalMsgToClient', res);
+	});
+
+});
 
 //===== MAIN APP ======
 setInterval(function() {
